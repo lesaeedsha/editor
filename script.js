d1a527c6397f6ce8ececcd5cc697728645e2ea20
lesaeedsha <149090853+lesaeedsha@users.noreply.github.com>
2026-08-16 18:46:48 +0330

    version 2.0

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 const textarea = document.querySelector("textarea");
+const addSpaceButton = document.querySelector("#add-space");
+const spaceContainer = document.querySelector("#space-container");
 
-textarea.addEventListener("keydown", (e) => {
-  if (e.key === "Escape") {
-    textarea.value = "";
+
+addSpaceButton.addEventListener("click", addSpace);
+
+function addSpace() {
+  const spaceWrapper = document.createElement("div");
+  spaceWrapper.className = "space-wrapper";
+  const removeSpace = document.createElement("button");
+  removeSpace.className = "remove-space";
+  removeSpace.innerText = "×";
+  const space = document.createElement("textarea");
+  space.spellcheck = false;
+  space.placeholder = 'Type ...';
+  spaceWrapper.append(removeSpace, space);
+  spaceContainer.appendChild(spaceWrapper);
+}
+
+
+spaceContainer.addEventListener('click', (event) => {
+  if (event.target.classList.contains('remove-space')) {
+    const wrapper = event.target.closest('.space-wrapper');
+    wrapper.remove();
   }
 });
+
